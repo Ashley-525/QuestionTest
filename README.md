a57f35f64e36a978faa60fae2ee19a93a694a846
Ashley <Ashley.Barker@traineeacademy.com>
2020-04-09 16:37:24 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # QuestionTest
 # QuestionTest
+# QuestionTest
---
+++ src/main/java/com/qa/testdrivendevelopment/Questions.java
@@ -0,0 +1,300 @@
+package com.qa.testdrivendevelopment;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import person.Person;
+
+public class Questions {
+
+	/**
+	 * EXAMPLE: THIS ONE HAS BEEN DONE FOR YOU <br>
+	 * 
+	 * Given a name, return "Hi" plus the name <br>
+	 * <br>
+	 * For Example:<br>
+	 * greetingExample("John") → "Hi John" <br>
+	 * greetingExample("Matt") → "Hi Matt" <br>
+	 * greetingExample("Angelica") → "Hi Angelica"
+	 * 
+	 */
+	public String greetingExample(String name) {
+		return "Hi " + name;
+	}
+
+	/**
+	 * Given a string, return a string where for every char in the original string,
+	 * there are three chars.<br>
+	 * <br>
+	 * 
+	 * For Example:<br>
+	 * multChar("The") → "TTThhheee"<br>
+	 * multChar("AAbb") → "AAAAAAbbbbbb"<br>
+	 * multChar("Hi-There") → "HHHiii---TTThhheeerrreee"
+	 */
+	public String multiChar(String input) {
+		return input;
+	}
+
+	/**
+	 * Return the string (backwards) that is between the first and last appearance
+	 * of "bread" in the given string, or return the empty string "" if there is not
+	 * 2 occurances of "bread". Ignore Case<br>
+	 * <br>
+	 * For Example: sandwichFilling("breadclivebread") → "evilc"<br>
+	 * sandwichFilling("xxbreadfridgebreadyy") → "egdirf"<br>
+	 * sandwichFilling("xxBreadfridgeBReAdyy") → "egdirf"<br>
+	 * sandwichFilling("xxbreadyy") → "" sandwichFilling("xxbreADyy") → ""<br>
+	 * <br>
+	 * HINT: "a" == "a" if false HINT: "a".equals("a") is true
+	 */
+
+	public String sandwichFilling(String sandwich) {
+		Scanner read = new Scanner(System.in);
+		String s = "xtbreadbreadx";
+		String reverse = "";
+		
+
+		
+		System.out.println("Reversed string containing 2 bread indexes is: ");
+//		
+//		System.out.println(str1.contains("bread"));
+//		
+		System.out.println(s.lastIndexOf(s));
+		System.out.println(s.indexOf("bread"));
+		System.out.println(s);
+		
+
+//		
+		if (s.indexOf("bread") == 2) {
+			for(int i = s.length() -1; i>=0; i--) {
+				reverse = reverse + s.indexOf("bread");
+			}
+			
+			
+			//System.out.println(s.indexOf("bread"));
+			return s;
+		} else {
+			return "";
+		}
+		
+		
+//		super();
+//		this.sandwich = sandwich;
+//		
+//		public String sandwich() {
+//			//return name();
+//			System.out.println(s.contains("bread"));
+//			
+//			return s;
+//	}
+		
+		
+		
+	}
+
+	/**
+	 * Given three ints, a b c, one of them is small, one is medium and one is
+	 * large. Return true if the three values are evenly spaced, so the difference
+	 * between small and medium is the same as the difference between medium and
+	 * large. Do not assume the ints will come to you in a reasonable order.<br>
+	 * <br>
+	 * For Example:<br>
+	 * evenlySpaced(2, 4, 6) → true<br>
+	 * evenlySpaced(4, 6, 2) → true<br>
+	 * evenlySpaced(4, 6, 3) → false<br>
+	 * evenlySpaced(4, 60, 9) → false
+	 */
+	public boolean evenlySpaced(int a, int b, int c) {
+		return false;
+	}
+
+	/**
+	 * Given a string and an int n, return a string that removes n letters from
+	 * the'middle' of the string. The string length will be at least n. The input
+	 * string will have an odd number of letters. n will always be an odd
+	 * number.<br>
+	 * <br>
+	 * For Example:<br>
+	 * nMid("Hello", 3) → "Ho"<br>
+	 * nMid("Chocolate", 3) → "Choate"<br>
+	 * nMid("Chocolate", 1) → "Choclate"<br>
+	 */
+//	private String input;
+//	private int n;
+	
+	
+	public String nMid(String input, int n) {
+//		this.input = input;
+		input = "Chocolate";
+		n = 3;
+//		this.n = n;
+		String stri = input;
+		String stri_ = stri.substring(0, stri.length()-n);
+		System.out.println(stri_);
+		
+		
+//		String input = "xtbreadbreadx";
+//		int n = 3;
+//		
+//		System.out.println(("bread"));
+
+    	return stri_;
+	}
+
+	/**
+	 * Given a string, return the boolean True if it ends in "java" and False if
+	 * does not end in "java". Ignore Case.<br>
+	 * <br>
+	 * For Example: <br>
+	 * endsJava("ilovejava") → true <br>
+	 * endsJava("iloveJaVa") → true <br>
+	 * endsJava("welovejava") → true <br>
+	 * endsJava("welovejavamoreeveryday") → false <br>
+	 * endsJava("javaiscool") → false <br>
+	 * endsJava("pythoniscool") → false <br>
+	 */
+	public boolean endsJava(String input) {
+		input = "hellojava";
+		if (input.endsWith("java")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Given a string, return the length of the largest "block" in the string. A
+	 * block is a run of adjacent chars that are the same, do not ignore case.<br>
+	 * <br>
+	 * For Example: superBlock("hooopplla") → 3 <br>
+	 * superBlock("abbCCCddDDDDeeEEE") → 4 <br>
+	 * superBlock("abbCCCCC")) → 5 <br>
+	 * superBlock("") → 0 <br>
+	 * <br>
+	 * HINT: "a" == "a" if false HINT: "a".equals("a") is true
+	 */
+	public int superBlock(String input) {
+		String strname = "abc";
+		int count = 0;
+		int maxc = 0;
+		
+		for(int i=0; i<strname.length(); i++){
+			
+			if(i==0){
+				count++;
+				maxc = count;
+			}
+			else{
+				if(strname.charAt(i)==strname.charAt(i-1)){
+					count++;
+					if(maxc<count){
+						maxc = count;
+					}
+				}//end of if
+				
+				else{
+					count = 1;
+				}
+			}
+			
+		}
+		
+		return maxc;
+	}
+
+	/**
+	 * Given a string - return the number of times "am" appears in the String
+	 * ignoring case - BUT ONLY WHEN the word "am" appears without being followed or
+	 * preceded by other letters <br>
+	 * <br>
+	 * For Example: <br>
+	 * amISearch("Am I in Amsterdam") → 1 <br>
+	 * amISearch("I am in Amsterdam am I?") → 2 <br>
+	 * amISearch("I have been in Amsterdam") → 0 <br>
+	 * <br>
+	 * HINT: String.toLowerCase
+	 */
+	public int amISearch(String sentence) {
+    	return -1;
+	}
+
+	/**
+	 * Given a number if this number is divisible by 3 return "fizz" if this number
+	 * is divisible by 5 return "buzz" if this number is divisible by both 3 and 5
+	 * return "fizzbuzz" if this number is not divisible by 3 or 5 return null<br>
+	 * <br>
+	 * For Example: <br>
+	 * fizzBuzz(3) → "fizz" <br>
+	 * fizzBuzz(10) → "buzz" <br>
+	 * fizzBuzz(15) → "fizzbuzz" <br>
+	 * fizzBuzz(8) → null
+	 */
+	public String fizzBuzz(int number) {
+    	return "";
+	}
+
+	/**
+	 * Given a string, split the string into the individual numbers present then add
+	 * each digit of each number to get a final value for each number <br>
+	 * <br>
+	 * String example = "55 72 86"<br>
+	 * <br>
+	 * "55" will = the integer 10 <br>
+	 * "72" will = the integer 9 <br>
+	 * "86" will = the integer 14 <br>
+	 * <br>
+	 * You then need to return the highest vale <br>
+	 * <br>
+	 * For Example: <br>
+	 * largest("55 72 86") → 14 <br>
+	 * largest("15 72 80 164") → 11 <br>
+	 * largest("555 72 86 45 10") → 15 <br>
+	 * <br>
+	 * HINT: Integer.parseInt
+	 */
+
+	public int largest(String input) {
+//		input = "100,23";
+//		ArrayList<Integer> ints = Arrays.stream(line.split(","))
+//		        .map(Integer::parseInt)
+//		        .collect(Collectors.toList());
+		
+		input = "1,21,33";
+		ArrayList<Integer> ints = Arrays.stream(line.split(","))
+		        .map(Integer::parseInt)
+		        .collect(Collectors.toList());
+		
+		
+    	return -1;
+	}
+
+	/**
+	 * Given a string, int and a char, return a boolean value if the 'nth'
+	 * (represented by the int provided) char of the String supplied is the same as
+	 * the char supplied. The int provided will NOT always be less than than the
+	 * length of the String. Ignore case and Whitespace. <br>
+	 * <br>
+	 * For Example: <br>
+	 * compares("The",2,'h') → true <br>
+	 * compares("AAbb",1,'b') → false <br>
+	 * compares("Hi-There",10,'e') → false <br>
+	 * <br>
+	 * HINT: String.charAt
+	 */
+	public boolean compares(String word, int index, char letter) {
+		word = "The";
+		index = 2;
+		letter = 8;
+		
+		if (word.length()) {
+			
+			return true;
+			
+		}
+		
+    	return false;
+	}
+}
